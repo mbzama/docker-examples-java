4371675eda6a4cb48a3307ba5ea1e8da7e6c01f1
zama <bmohammad@evoketechnologies.com>
2019-07-24 23:25:24 +0530

    Add java standalone

---
+++ standalone/src/main/java/zama/training/standalone/App.java
@@ -0,0 +1,18 @@
+package zama.training.standalone;
+
+import java.util.Date;
+
+public class App {
+	private static Integer counter = 0;
+	
+	public static void main(String[] args) throws InterruptedException {
+		String javaVersion = "Java version: "+System.getProperty("java.version")+" - ";
+		
+		while(true){
+			System.out.println(javaVersion + new Date() +" - Event: "+counter );
+			counter++;
+			Thread.sleep(3000);
+		}
+	}
+
+}
