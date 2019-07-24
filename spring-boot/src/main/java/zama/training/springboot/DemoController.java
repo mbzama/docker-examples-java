3517c35de6095f5bb98e68ba2dfdc58a61f1328b
zama <bmohammad@evoketechnologies.com>
2019-07-25 00:49:40 +0530

    spring-boot docker

---
+++ spring-boot/src/main/java/zama/training/springboot/DemoController.java
@@ -0,0 +1,26 @@
+package zama.training.springboot;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    private static final String template = "Welcome to Training - %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greet")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Learner") String name) {
+    	logger.info("Processing request for /greet --> "+name);
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
