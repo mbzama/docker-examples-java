798946076defbaf1d9f65d92fbe84d3d657bfe39
mbzama <zamamb@gmail.com>
2019-08-20 16:27:13 +0530

    Add IP address for k8s demo

--- spring-boot/src/main/java/zama/training/springboot/Greeting.java
+++ spring-boot/src/main/java/zama/training/springboot/Greeting.java
@@ -1,20 +1,48 @@
 package zama.training.springboot;
 
+/**
+ * 
+ * @author zama
+ *
+ */
 public class Greeting {
 
-	private final long id;
-	private final String content;
+	private long id;
+	private String ipAddress;
+	private String content;
 
-	public Greeting(long id, String content) {
+	public Greeting(long id, String content, String ipAddress) {
 		this.id = id;
 		this.content = content;
+		this.ipAddress = ipAddress;
 	}
 
 	public long getId() {
 		return id;
 	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
 	public String getContent() {
 		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Greeting [id=" + id + ", ipAddress=" + ipAddress + ", content=" + content + "]";
 	}
 }
