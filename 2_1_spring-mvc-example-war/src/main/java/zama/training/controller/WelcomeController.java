b043a1802de43411c1e2f53829853e1926927eb2
mdbadiyuzamaevoke <bmohammad@evoketechnologies.com>
2020-09-02 19:20:30 +0530

    Add war(spring mvc+thymeleaf) example

---
+++ 2_1_spring-mvc-example-war/src/main/java/zama/training/controller/WelcomeController.java
@@ -0,0 +1,37 @@
+package zama.training.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class WelcomeController {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
+
+}
