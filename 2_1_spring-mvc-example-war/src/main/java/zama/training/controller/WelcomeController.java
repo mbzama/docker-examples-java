1389a9e087a4af1eca361992c9092e13c0224f4d
mdbadiyuzamaevoke <zamamb@gmail.com>
2020-09-30 21:13:38 +0530

    Load properties

--- 2_1_spring-mvc-example-war/src/main/java/zama/training/controller/WelcomeController.java
+++ 2_1_spring-mvc-example-war/src/main/java/zama/training/controller/WelcomeController.java
@@ -1,17 +1,25 @@
 package zama.training.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class WelcomeController {
-	public static String PROPERTY_FILE_LOCATION = "/shared/";
+    private static final Logger logger = LogManager.getLogger(WelcomeController.class);
+
+	public static String PROPERTY_FILE_LOCATION = "/shared/dev.properties";
 	
     @Value("${welcome.message}")
     private String message;
@@ -22,20 +30,38 @@ public class WelcomeController {
     public String main(Model model) {
         model.addAttribute("message", message);
         model.addAttribute("tasks", tasks);
-
+        addParamsToModel(model);
+        
         return "welcome"; //view
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/properties")
     public String mainWithParam(
             @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
-
-        model.addAttribute("message", name);
-
-        return "welcome";
+        addParamsToModel(model);
+        
+        return "properties";
     }
+
+	private void addParamsToModel(Model model) {
+		Properties prop = readPropertyFile();
+        model.addAttribute("driverName", prop.get("driverName"));
+        model.addAttribute("datasourceUrl", prop.get("datasourceUrl"));
+        model.addAttribute("datasourceUserName", prop.get("datasourceUserName"));
+        model.addAttribute("datasourcePassword", prop.get("datasourcePassword"));
+	}
     
-    private void readPropertyFile() {
-    	
+    private Properties readPropertyFile() {
+    	Properties props = new Properties();
+    	logger.info("Loading property file from: "+ PROPERTY_FILE_LOCATION);
+    	try(FileInputStream fis = new FileInputStream(PROPERTY_FILE_LOCATION)){
+    		props.load(fis);
+    		logger.info("props: "+props);
+    	} catch (FileNotFoundException e) {
+    		logger.error(e);
+		} catch (IOException e) {
+			logger.error(e);
+		}
+    	return props;
     }
 }
